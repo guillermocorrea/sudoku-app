12f4e419dae1fb5b44e3c6b0a74cfe8bbf9a1242
Luis Correa <luis.correag@perficient.com>
2021-06-10 20:57:50 -0500

    Clean up project

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './core/reportWebVitals';
+import { unregister } from './core';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>Hello World</div>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +14,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+unregister();
